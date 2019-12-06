f762a79861327477a00bf53bf587f79aaf75a968
Jonatan <jonatan.gall@ibm.com>
2019-12-06 01:35:02 -0300

    detail dialog and detail component

--- src/app/assets/asset-detail/asset-detail-dialog.component.ts
+++ src/app/assets/asset-detail/asset-detail-dialog.component.ts
@@ -4,16 +4,41 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'hyperiot-asset-detail-dialog',
   template: `
-    <h1 mat-dialog-title>Escolha um usuário da sua lista</h1>
+    <style>
+
+    </style>
+    <h1 mat-dialog-title>Autorização de usuário</h1>
+    <div mat-dialog-content *ngIf="!allowedUsers.length">
+      <p>Não há usuários ná sua lista
+      <small style="color: gray">Na seção Participantes é possível adiciona-los</small>
+      </p>
+    </div>
+
+    <div mat-dialog-content *ngIf="allowedUsers.length">
+      <mat-form-field>
+        <mat-label>Lista de usuários autorizados</mat-label>
+        <mat-select [(value)]="selectedUser">
+          <mat-option *ngFor="let item of allowedUsers" [value]="item">
+            {{item}}
+          </mat-option>
+        </mat-select>
+      </mat-form-field>
+    </div>
+    
     <div mat-dialog-actions>
       <button mat-button (click)="onNoClick()">Cancelar</button>
-      <button mat-button color="accent" [mat-dialog-close]="data" cdkFocusInitial>Confirmar</button>
+      <button mat-button color="accent" [mat-dialog-close]="selectedUser" cdkFocusInitial>Confirmar</button>
     </div>
   `
 })
 export class AssetDetailDialogComponent {
+  selectedUser;
   allowedUsers = [];
   constructor(public dialogRef: MatDialogRef<AssetDetailDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any ) { }
-  
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
+      this.allowedUsers = this.data || [];
+    }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/assets/asset-detail/asset-detail.component.ts
+++ src/app/assets/asset-detail/asset-detail.component.ts
@@ -1,3 +1,5 @@
+import { ParticipantsService } from './../../participants/participants.service';
+import { AssetsService } from './../assets.service';
 import { AssetDetailDialogComponent } from './asset-detail-dialog.component';
 import { NotificationService } from './../../core/services/notification.service';
 import { ActivatedRoute } from '@angular/router';
@@ -11,9 +13,13 @@ import { MatDialog } from '@angular/material';
 })
 export class AssetDetailComponent implements OnInit {
   public assetDetail;
+  public ownerList;
+  public loading = false;
   constructor(public route: ActivatedRoute,
+              public assetService: AssetsService,
               public notification: NotificationService,
-              public dialog: MatDialog) { }
+              public dialog: MatDialog
+    ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(res => {
@@ -23,6 +29,11 @@ export class AssetDetailComponent implements OnInit {
         return;
       }
       this.assetDetail = JSON.parse(asset);
+      this.loading = true;
+      this.assetService.getOwnerDetails().subscribe(res => {
+        this.ownerList = res;
+        this.loading = false;
+      });
     })
   }
   getEmail(email = '#') {
@@ -33,12 +44,37 @@ export class AssetDetailComponent implements OnInit {
     console.log(allowedUser);
     const dialogRef = this.dialog.open(AssetDetailDialogComponent, {
       width: '250px',
-      data: allowedUser
+      data: this.ownerList.map(el => el.email)
     });
+  }
+  filterAllowedUsers() {
+    (this.assetDetail.allowedUsers || []).filter(el => !this.assetDetail.owner);
   }
   addUser(){
     const dialogRef = this.dialog.open(AssetDetailDialogComponent, {
-      width: '250px'
+      width: '500px',
+      data: this.ownerList.map(el => el.email)
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (!!result) {
+        const transaction = {
+          device: this.assetDetail.commodityId,
+          allowedUser: result,
+          owner: this.assetDetail.owner
+        }
+        console.log('submit ==> ', transaction);
+        
+        // const {name} = result;
+        // this.ps.removeParticipant(result.email)
+        //   .subscribe(res => {
+        //     this.allowedUsers.splice(this.allowedUsers.indexOf(result), 1);
+        //     this.nf.notify(`${name}, Removido!`);
+        //     this.loading = false;
+        //   }, err => {
+        //     this.nf.notify('Erro na transação.');
+        //     this.loading = false;
+        //   })
+      }
     });
   }
 
